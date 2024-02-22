634289614babfb8ecf31bc908972678287f7b4f8
Flavia <flavia.avila.pereira@gmail.com>
2024-02-21 21:42:42 -0300

    refat

--- src/app/views/game/game.module.ts
+++ src/app/views/game/game.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MaterialModule } from 'src/app/material.module';
 import { GameComponent } from './game/game.component';
 import { MemoryCardComponent } from './memory-card/memory-card.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
