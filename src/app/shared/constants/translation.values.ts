756fb6de1863113d44749f84349310a82c988e20
Flavia <flavia.avila.pereira@gmail.com>
2024-02-22 19:13:19 -0300

    Create translation.values.ts

---
+++ src/app/shared/constants/translation.values.ts
@@ -0,0 +1,36 @@
+export const TRANSLATION = {
+    title: {
+        pt: 'JOGO DA MEMÓRIA',
+        en: 'MEMORY GAME'
+    },
+    answer: {
+        yes: {
+            pt: 'Sim',
+            en: 'Yes'
+        },
+        no: {
+            pt: 'Não',
+            en: 'No'
+        }
+    },
+    gameIsNotOver: {
+        pt: 'Espere! O jogo ainda não acabou!',
+        en: 'Hang on! The game is not over yet!'
+    },
+    startNewGameConfirmation: {
+        pt: 'Tem certeza que deseja iniciar novo jogo?',
+        en: 'Are you sure you want to start a new game?'
+    },
+    liveGameConfirmation: {
+        pt: 'Tem certeza que deseja sair do jogo?', 
+        en: 'Are you sure you want to exit the game?'
+    },
+    congratulatios: {
+        pt: 'Parabéns!', 
+        en: 'Congratulations!'
+    },
+    playAgainConfirmation: {
+        pt: 'Deseja jogar novamente?',
+        en: 'Do you want to play again?'
+    },
+}
