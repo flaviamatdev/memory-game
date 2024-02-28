ef949a59d093399cec85914f0ae067ac3a578863
Flavia <flavia.avila.pereira@gmail.com>
2024-02-28 13:38:16 -0300

    add imagens dos tipos de cardId na home

--- src/app/views/home/home/home.component.ts
+++ src/app/views/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CardIdTypeEnum, CardIdTypeName } from 'src/app/shared/enums/card-id-type.enum';
 
 @Component({
     selector: 'app-home',
@@ -8,7 +9,42 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
+    cardIds: any[] = [];
+
     constructor(private router: Router) { }
+
+    ngOnInit() {
+        this._setCardIdInfo();
+    }
+
+    private _setCardIdInfo() {
+        const imgPath = 'assets/images/card-id/card_id_';
+        const cardIdTypeName = CardIdTypeName;
+        
+        this.cardIds = [
+            {
+                src: `${imgPath}numbers.png`,
+                text: {
+                    title: cardIdTypeName[CardIdTypeEnum.NUMBERS],
+                    bottom: 'padrão'
+                }
+            },
+            {
+                src: `${imgPath}row_column.png`,
+                text: {
+                    title: cardIdTypeName[CardIdTypeEnum.ROW_COLUMN],
+                    bottom: 'explorar localização no plano'
+                }
+            },
+            {
+                src: `${imgPath}icons.png`,
+                text: {
+                    title: cardIdTypeName[CardIdTypeEnum.ICONS],
+                    bottom: 'para crianças ainda não alfabetizadas'
+                }
+            }
+        ]
+    }
 
     buildGame() {
         this.router.navigateByUrl('builder');
