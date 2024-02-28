6c24d3140be1eb8f915e0bd4b5c70979ef06eff0
Flavia <flavia.avila.pereira@gmail.com>
2024-02-28 12:01:13 -0300

    add font Nunito + uso em varios componentes

--- src/app/views/game-builder/game-builder/game-builder.component.ts
+++ src/app/views/game-builder/game-builder/game-builder.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-game-builder',
     templateUrl: './game-builder.component.html',
-    styleUrls: []
+    styleUrls: ['./game-builder.component.scss']
 })
 export class GameBuilderComponent {
 }
