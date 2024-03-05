5ac780b24a3dbc5d8c73e64c47218b3b0f25a4b0
Flavia <flavia.avila.pereira@gmail.com>
2024-03-05 12:20:18 -0300

    add traducao comp credits

--- src/app/views/credits/credits/credits.component.ts
+++ src/app/views/credits/credits/credits.component.ts
@@ -7,13 +7,24 @@ import { Component } from '@angular/core';
 })
 export class CreditsComponent {
 
+    readonly TITLE = {
+        pt: 'Créditos',
+        en: 'Credits'
+    }
+
     readonly CREDITS = [
         {
-            text: 'Código fonte do controle do jogo e das animações adaptado de ',
+            text: {
+                pt: 'Código fonte do controle do jogo e das animações adaptado de: ',
+                en: 'Source code for game control and animations adapted from: '
+            },
             url: 'https://bariscanyilmaz.github.io/memory-game-angular'
         },
         {
-            text: 'Efeitos sonoros: ',
+            text: {
+                pt: 'Efeitos sonoros: ',
+                en: 'Sound effects: '
+            },
             url: 'https://pixabay.com/sound-effects'
         },
     ];
