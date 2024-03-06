7f3df843c3511c4f4c87cc9388bc701b5ef24748
Flavia <flavia.avila.pereira@gmail.com>
2024-03-05 21:17:25 -0300

    adtraducoes yes/no nos jsons

--- src/app/shared/components/dialog/dialog-values.ts
+++ src/app/shared/components/dialog/dialog-values.ts
@@ -1,14 +1,4 @@
 export const DIALOG_TRANSLATION = {
-    confirmationBtn: {
-        yes: {
-            pt: 'Sim',
-            en: 'Yes'
-        },
-        no: {
-            pt: 'Não',
-            en: 'No'
-        }
-    },
 
     gameIsNotOverWarning: {
         pt: 'Espere! O jogo ainda não acabou!',
@@ -19,4 +9,5 @@ export const DIALOG_TRANSLATION = {
         pt: 'Tem certeza que deseja sair do jogo?',
         en: 'Are you sure you want to exit the game?'
     },
+    
 }
