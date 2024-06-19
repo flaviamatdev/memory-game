f70e90de0df4fb13eabe146e0c81b556699ad0b8
Flavia <31422365+flaviamatdev@users.noreply.github.com>
2024-06-18 22:17:43 -0300

    [custom-sounds] ajuste texto tela Creditos

--- src/app/views/credits/credits/credits.component.ts
+++ src/app/views/credits/credits/credits.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITranslation } from 'src/app/shared/components/translation/translation.model';
 
 @Component({
     selector: 'app-credits',
@@ -10,21 +11,21 @@ export class CreditsComponent {
     readonly TITLE = {
         pt: 'Créditos',
         en: 'Credits'
-    }
+    } as ITranslation;
 
     readonly CREDITS = [
         {
             text: {
                 pt: 'Código fonte do controle do jogo e das animações adaptado de: ',
                 en: 'Source code for game control and animations adapted from: '
-            },
+            } as ITranslation,
             url: 'https://bariscanyilmaz.github.io/memory-game-angular'
         },
         {
             text: {
-                pt: 'Efeitos sonoros: ',
-                en: 'Sound effects: '
-            },
+                pt: 'Efeitos sonoros (não customizados): ',
+                en: 'Sound effects (not customized): '
+            } as ITranslation,
             url: 'https://pixabay.com/sound-effects'
         },
     ];
